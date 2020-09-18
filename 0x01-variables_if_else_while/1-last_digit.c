af47bf471f2e676911b68578c469c493699bca91
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-18 01:21:41 +0000

    Betty 80 character line break

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,12 +14,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	modulo = n % 10;
+	mo = n % 10;
 
 	if (n > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, mo);
 	else if (n == 0)
-		printf("Last digit of %d is %d and is 0\n", n, modulo);
+		printf("Last digit of %d is %d and is 0\n", n, mo);
 	else
 		printf("Last digit of %d is %d and is less than 6 and not 0\n",
 		       n, mo);
