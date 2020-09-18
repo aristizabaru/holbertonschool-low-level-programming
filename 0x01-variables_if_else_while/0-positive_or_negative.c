e8574204974d8fb90d17e01efd25c06fd90fc932
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-18 00:03:21 +0000

    Betty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,16 +10,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
 		printf("%d is positive\n", n);
-	}
-	if (n == 0)
-	{
+	else if (n == 0)
 		printf("%d is zero\n", n);
-	}
-	if (n < 0)
-	{
+	else (n < 0)
 		printf("%d is negative\n", n);
-	}
 	return (0);
 }
