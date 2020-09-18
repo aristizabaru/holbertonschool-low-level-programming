2918407ca4962fe8f49a258b07ff359404ec7b32
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-18 23:34:13 +0000

    new line

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Main - Entry point
+ * Description: print text
+ * Return 1
+ */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
