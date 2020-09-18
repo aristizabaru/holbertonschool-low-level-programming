3f49233315a652ddb2e6dd7ced0654d7ff9e64d6
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-18 00:00:04 +0000

    Only if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,11 +13,11 @@ int main(void)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n == 0)
+	if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
