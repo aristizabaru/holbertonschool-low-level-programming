7a02d668368eb0aad62bac687da6882900d05e08
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-18 01:44:00 +0000

    task upload

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Prints a..z
+ * Return: 0
+ */
+int main(void)
+{
+	char abc = 'a';
+
+	while (abc <= 'z'){
+		putchar(abc);
+		abc++;
+	}
+	putchar('\n');
+	return (0);
+}
