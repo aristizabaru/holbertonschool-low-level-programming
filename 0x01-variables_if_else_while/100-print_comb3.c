c53b2867bcb7b6c731a722986d347514c98f8c56
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-18 04:18:02 +0000

    nested if

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - main blocm
+ * Description: Numbers 00..99
+ * Return: 0
+ */
+int main(void)
+{
+	int i, d, m;
+	int c = 1;
+
+	for (i = 0; i < 100; i++)
+	{
+		d = i / 10;
+		m = i % 10;
+		if (m <= 9)
+		{
+			m += c;
+			if (m == 9)
+				c++;
+		}
+		if (m <= 9)
+		{
+			putchar(d + '0');
+			putchar(m + '0');
+
+			if (i < 99)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
