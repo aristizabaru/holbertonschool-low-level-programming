de5235266e9a4e66fd37d5691b8a099d1aac4be6
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-19 02:38:02 +0000

    upload task

---
+++ 0x00-hello_world/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: write text
+ * return 1
+ */
+int main(void)
+{
+	int n1, n2, n3;
+	int b2 = 1;
+	int b3 = 2;
+
+	for (n1 = 0; n1 <= 9; n1++)
+	{
+		for (n2 = b2; n2 <= 9; n2++)
+		{
+			for (n3 = b3; n3 <= 9; n3++)
+			{
+				putchar(n1 + '0');
+				putchar(n2 + '0');
+				putchar(n3 + '0');
+				putchar(44);
+				putchar(32);
+			}
+			b3++;
+		}
+		b2++;
+		b3 = b2 + 1;
+	}
+}
