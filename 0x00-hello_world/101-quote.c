8b45d6a1ff0fe00f64bc668597b08c4e3d8d154d
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-19 00:08:31 +0000

    use write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,7 @@
+#include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
-/**
- * Main - Entry point
- * Description: print text
- * Return 1
- */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 	return (1);
 }
