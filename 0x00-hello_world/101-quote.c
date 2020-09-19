0752cffe273aa7d4efae854b74ff4c8c4489f9d1
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-19 00:12:07 +0000

    change redirection

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,6 +2,6 @@
 #include <stdio.h>
 int main(void)
 {
-	write(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 	return (1);
 }
