4e4db9b7467ce99858a77c54193699e8136b62b1
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-20 14:40:45 +0000

    Comment functions

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+
+/**
+ * print - print numbers
+ * Description: print numbers combinations
+ * checking no repeat
+ * @Ap: Primer digito pointer - primer número
+ * @Bp: Segundo digito pointer - primer número
+ * @A2p: Primer digito pointer - segundo número
+ * @B2p: Segundo digito pointer - segundo número
+ * Return: 0
+ */
+int print(int *Ap, int *Bp, int *A2p, int *B2p)
+{
+	int A = *Ap;
+	int B = *Bp;
+	int A2 = *A2p;
+	int B2 = *B2p;
+
+	if (A <= A2 && (A + B <= A2 + B2))
+	{
+		if (!(A == A2 && B == B2))
+		{
+			putchar(A + '0');
+			putchar(B + '0');
+			putchar(32);
+			putchar(A2 + '0');
+			putchar(B2 + '0');
+			if (!((A == 9 && B == 8) && (A2 == 9 && B2 == 9)))
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	return (0);
+}
+
 /**
  * main - entry point
- * Description: combinations 3 digits
+ * Description: gets all numbers combinations
  * Return: 0
  */
 int main(void)
@@ -16,22 +53,7 @@ int main(void)
 			{
 				for (B2 = 0; B2 <= 9; B2++)
 				{
-					if (A <= A2 && (A + B <= A2 + B2))
-					{
-						if (!(A == A2 && B == B2))
-						{
-							putchar(A + '0');
-							putchar(B + '0');
-							putchar(32);
-							putchar(A2 + '0');
-							putchar(B2 + '0');
-							if (!((A == 9 && B == 8) && (A2 == 9 && B2 == 9)))
-							{
-								putchar(44);
-								putchar(32);
-							}
-						}
-					}
+					print(&A, &B, &A2, &B2);
 				}
 			}
 		}
