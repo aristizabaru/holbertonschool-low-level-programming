7587eba9bbffb4cdd766c8de175c547d69d6d02a
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-20 12:18:47 +0000

    betty style

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,36 +6,33 @@
  */
 int main(void)
 {
-        int A, A2, B, B2;
+	int A, A2, B, B2;
 
-        for (A = 0; A <= 9; A++)
-        {
-                for (B = 0; B <= 9; B++)
-                {
-                        for (A2 = 0; A2 <= 9; A2++)
-                        {
-                                for (B2 = 0; B2 <= 9; B2++)
-                                {
-
-                                        if (A + B < A2 + B2)
-                                        {
-
-                                                putchar(A + '0');
-                                                putchar(B + '0');
-                                                putchar(32);
-                                                putchar(A2 + '0');
-                                                putchar(B2 + '0');
-                                                if (!((A == 9 && B == 8) && (A2 == 9 && B2 == 9)))
-                                                {
-                                                        putchar(44);
-                                                        putchar(32);
-                                                }
-                                        }
-                                }
-                        }
-                }
-        }
-
-        putchar('\n');
-        return (0);
+	for (A = 0; A <= 9; A++)
+	{
+		for (B = 0; B <= 9; B++)
+		{
+			for (A2 = 0; A2 <= 9; A2++)
+			{
+				for (B2 = 0; B2 <= 9; B2++)
+				{
+					if (A + B < A2 + B2)
+					{
+						putchar(A + '0');
+						putchar(B + '0');
+						putchar(32);
+						putchar(A2 + '0');
+						putchar(B2 + '0');
+						if (!((A == 9 && B == 8) && (A2 == 9 && B2 == 9)))
+						{
+							putchar(44);
+							putchar(32);
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
