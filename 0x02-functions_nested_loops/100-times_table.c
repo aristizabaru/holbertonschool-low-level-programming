3acff2d65b8ce925dec511f4c7a581ac6971e6f5
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-22 19:25:19 +0000

    organice functions

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@
  */
 void print_times_table(int n)
 {
-	int l, b, r, fd, md, ld;
+	int l, b;
 	int e = 0;
 
 	if (!(n > 15 || n < 0))
@@ -18,42 +18,56 @@ void print_times_table(int n)
 		{
 			for (b = 0; b < n + 1; b++)
 			{
-				r = b * e;
-				if (r == 0 && b == 0)
-					_putchar(r + '0');
-				else
-					_putchar(' ');
-				if (r < 10 && b != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(r + '0');
-				}
-				else if (r > 9 && r < 100 && b != 0)
-				{
-					fd = firstDigit(r);
-					ld = lastDigit(r);
-					_putchar(' ');
-					_putchar(fd + '0');
-					_putchar(ld + '0');
-				}
-				else if (r > 99)
-				{
-					fd = firstDigit(r);
-					md = middleDigit(r);
-					ld = lastDigit(r);
-					_putchar(fd + '0');
-					_putchar(md + '0');
-					_putchar(ld + '0');
-				}
-
-				if (b != n)
-					_putchar(',');
+				check(&b, &e, &n);
 			}
 			_putchar('\n');
 			e++;
 		}
 	}
+}
+
+/**
+ * check - check condition of table
+ * @b: base number
+ * @e: exponente
+ * @n: given number
+ *
+ * Return: void
+ */
+void check(int *b, int *e, int *n)
+{
+	int r, fd, md, ld;
+
+	r = *b * *e;
+	if (r == 0 && *b == 0)
+		_putchar(r + '0');
+	else
+		_putchar(' ');
+	if (r < 10 && *b != 0)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(r + '0');
+	}
+	else if (r > 9 && r < 100 && *b != 0)
+	{
+		fd = firstDigit(r);
+		ld = lastDigit(r);
+		_putchar(' ');
+		_putchar(fd + '0');
+		_putchar(ld + '0');
+	}
+	else if (r > 99)
+	{
+		fd = firstDigit(r);
+		md = middleDigit(r);
+		ld = lastDigit(r);
+		_putchar(fd + '0');
+		_putchar(md + '0');
+		_putchar(ld + '0');
+	}
+	if (*b != *n)
+		_putchar(',');
 }
 
 /**
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -14,3 +14,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 int middleDigit(int n);
+void check(int *b, int *e, int *n);
