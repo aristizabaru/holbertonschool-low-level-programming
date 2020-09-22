9df078ae1c1a21a040e124129167774e04df803c
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-22 04:34:44 +0000

    upload task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints from 00:00 to 23:59
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int h1, h2, m, s;
+
+	for (h1 = 48; h1 < 51; h1++)
+	{
+		for (h2 = 48; h2 < 58; h2++)
+		{
+			for (m = 48; m < 54; m++)
+			{
+				for (s = 48; s < 58; s++)
+				{
+
+					_putchar(h1);
+					_putchar(h2);
+					_putchar(32);
+					_putchar(58);
+					_putchar(32);
+					_putchar(m);
+					_putchar(s);
+					_putchar('\n');
+				}
+			}
+			if (h1 == 50 && h2 == 51)
+				break;
+		}
+	}
+}
