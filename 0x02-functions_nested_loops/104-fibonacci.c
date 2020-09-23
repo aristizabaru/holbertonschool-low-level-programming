b902d7b8c97911774f858a5465ffc60accd69428
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-23 02:54:34 +0000

    task upload

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - finubacci to 98
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 1;
+	long double ant2 = 1;
+	long double ant1 = 2;
+	long double act;
+
+	printf("1, ");
+	while (i < 98)
+	{
+		act = ant1 + act;
+		ant1 = ant2;
+		ant2 = act;
+		if (i != 97)
+			printf("%.0Lf, ", act);
+		else
+			printf("%.0Lf", act);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
