54550aaea0ed18cf3adefe54b215d14e91bb63a3
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-23 02:04:59 +0000

    task upload

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - finubacci to 50
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 1;
+	long int ant2 = 1;
+	long int ant1 = 2;
+	long int act;
+	printf("1, ");
+	while (i < 50)
+	{
+		act = ant1 + act;
+		ant1 = ant2;
+		ant2 = act;
+		if (i != 49)
+			printf("%ld, ", act);
+		else
+			printf("%ld", act);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
