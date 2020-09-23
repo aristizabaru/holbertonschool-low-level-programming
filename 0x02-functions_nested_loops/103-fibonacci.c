c6b6db3c2fc254128e141809a71f6c0f4e3ef563
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-23 02:30:36 +0000

    change logic

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,17 +9,16 @@ int main(void)
 	int i = 1;
 	long int ant2 = 1;
 	long int ant1 = 2;
-	long int act;
+	long int act = 1;
 	long int res = 0;
 
-	while (i < 4000000)
+	while (act < 4000000)
 	{
 		act = ant1 + act;
 		ant1 = ant2;
 		ant2 = act;
 		if (act % 2 == 0)
 			res += act;
-		i++;
 	}
 	printf("%ld\n", res);
 	return (0);
