a924e828af557877c1550c0a8aa1ddcba47e17f2
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-23 00:49:56 +0000

    add return

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -15,15 +15,16 @@ int main(void)
 	{
 		/* Multiplos 3 */
 		m3 = 3 * i;
+		/* Multiplos 5 */
+		m5 = 5 * i;
+
 		if (m3 < 1024)
 			r3 += m3;
-		/* Multiplos 3 */
-		m5 = 5 * i;
+
 		if (m5 < 1024)
 			r5 += m5;
 		i++;
 	}
 	r = r3 + r5;
 	printf("%d\n", r);
-	return (0);
 }
