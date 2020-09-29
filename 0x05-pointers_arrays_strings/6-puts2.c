3a10f062cc2ea80d567153c33bb44ac1ede2919f
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-29 12:30:59 +0000

    task upload

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to print
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
