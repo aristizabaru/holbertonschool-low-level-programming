25aab6fdd3de74e3a641eda1e8822c8dff0e204b
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-29 11:33:12 +0000

    betty comment

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,13 +2,14 @@
 
 /**
  * _strlen - returns the length of a string
- * @*s: string to be evaluated
- * 
+ * @s: string to be evaluated
+ *
  * Return: lenght of the string
  */
 int _strlen(char *s)
 {
 	int count = 0;
+
 	while (*s)
 	{
 		s++;
