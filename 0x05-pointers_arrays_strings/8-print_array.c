4bc98d1c5a99dd189da5b12987dfb898a62c1f2f
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-29 20:40:05 +0000

    add stdio.h

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 
 /**
  * print_array -  prints n elements of an array of integers
@@ -12,11 +12,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n + 1; i++)
 	{
-		if (i < n)
-			printf("%d, ", a[i]);
-		else
-			printf("%d\n", a[i]);
+		printf("%d", a[i]);
+		if (a[i] != a[n - 1])
+			printf(", ");
 	}
+	printf("\n");
 }
