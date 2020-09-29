262d624761704865fb9e55ce1eabff49bca58fb3
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-29 17:13:12 +0000

    change logic

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,16 +14,8 @@ void puts_half(char *str)
 	for (length = 0; str[length] != '\0'; length++)
 		;
 
-	if (length % 2 == 0)
-	{
-		for (i = length / 2; i < length; i++)
-			_putchar(str[i]);
-	}
-	else
-	{
-		for (i = (length - 1) / 2; i < length; i++)
-			_putchar(str[i]);
-	}
+	for (i = length / 2; i <= length; i++)
+		_putchar(str[i]);
 
 	_putchar('\n');
 }
