873cd560d853c1c41f563cbd25ec45d7f18adc88
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-30 03:14:45 +0000

    change logic

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,29 +8,27 @@
  */
 int _atoi(char *s)
 {
-	int i;
-	int h, p;
+	int i = 0, res = 0, sign = -1;
 
-	h = 0;
-	p = -1;
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s)
 	{
 		if (s[i] == '-')
-			p *= -1;
+			sign *= -1;
 
 		if (s[i] > 47 && s[i] < 58)
 		{
-			if (h < 0)
-				h = (h * 10) - (s[i] - '0');
+			if (res < 0)
+				res = (res * 10) - (s[i] - '0');
 			else
-				h = (s[i] - '0') * -1;
+				res = (s[i] - '0') * -1;
 
 			if (s[i + 1] < 48 || s[i + 1] > 57)
 				break;
 		}
+		i++;
 	}
-	if (p < 0)
-		h *= -1;
+	if (sign < 0)
+		res *= -1;
 
-	return (h);
+	return (res);
 }
