7a8ff7e9d5c5c0d09330bb5ec366fefadd24525a
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-09-30 00:53:28 +0000

    task upload

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * _atoi -  convert a string to an integer.
+ * @s: string to convert
+ *
+ * Return: the first number in the string
+ */
+int _atoi(char *s)
+{
+	int res = 0, signo = 1;
+
+	while (*s)
+	{
+		/* compruebo si hay - o + y multiplica */
+		if (*s == '-')
+			signo *= -1;
+		else if (*s == '+')
+			signo *= 1;
+
+		/* imprime números */
+		if (*s >= '0' && *s <= '9')
+		{
+			/* Multiplica x 10 para agregar decena */
+			res = res * 10 + *s - '0';
+		}
+
+		/* salgo del loop cuando pasa número */
+		if (!(s[1] >= '0' && s[1] <= '9') && (s[0] >= '0' && s[0] <= '9'))
+			break;
+		s++;
+	}
+	return (res * signo);
+}
