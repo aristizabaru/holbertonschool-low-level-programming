01eb32746bd82c35b3bb0523ed5127669c3deb07
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-10-10 17:55:15 +0000

    3-mul // get rid of int r;

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,15 +9,13 @@
  */
 int main(int argc, char **argv)
 {
-	int r;
 
 	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	r = atoi(argv[1]) * atoi(argv[2]);
-	printf("%d\n", r);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 	return (0);
 }
