92821f33e1e033173e2be2d177ac9e1ddab91306
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-10-10 19:08:38 +0000

    4-add // change if and read content of pointer

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,31 +7,26 @@
  *
  * Return: 0 on success, 1 on error
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int r = 0, i, c = 0;
-	/*check if there's two arguments*/
-	if (argc == 1)
-		printf("0\n");
-	else
-	{
-		/*check for numbers and add*/
-		for (i = 1; i < argc; i++)
-		{
-			if (atoi(argv[i]) != 0 || *argv[i] == '0')
-				r += atoi(argv[i]);
-			else
-				c = 1;
-		}
-		/*print if there's no number*/
-		if (c == 1)
-		{
-			printf("Error\n");
-			return (c);
-		}
-		/*print add of r if ok*/
-		printf("%d\n", r);
-	}
+	int r = 0, i, j;
 
+	/*checks every letter of string for character*/
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		r += atoi(argv[i]);
+	}
+	printf("%d\n", r);
 	return (0);
 }
