757923fe4c94d0757555b9d5e456c89bfb8e75c3
Andrés Aristizábal <69760731+aristizabaru@users.noreply.github.com>
2020-10-10 12:43:01 -0500

    3-mul // return 1 on error

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,15 +9,18 @@
  */
 int main(int argc, char **argv)
 {
-	int r;
+    int r;
 
-	if (argc < 2)
-		printf("Error\n");
-	else
-	{
-		r = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", r);
-	}
+    if (argc < 2)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    else
+    {
+        r = atoi(argv[1]) * atoi(argv[2]);
+        printf("%d\n", r);
+    }
 
-	return (0);
+    return (0);
 }
