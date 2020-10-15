3681c1638181d78f386c3a02e9d620d8e144954d
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-10-15 19:25:48 +0000

    update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,18 +12,18 @@ int *array_range(int min, int max)
 {
 	int *p = NULL;
 	int i;
-	int length = max - min;
+	int length = max - min + 1;
 
 	/*check arguments*/
 	if (min > max)
 		return (NULL);
 	/*allocate memory*/
-	p = malloc(sizeof(*p) * length + 1);
+	p = malloc(sizeof(*p) * length);
 	/*check fail*/
 	if (p == NULL)
 		return (NULL);
 	/*populate memory from min to max*/
-	for (i = 0; i <= length; i++, min++)
+	for (i = 0; i < length; i++, min++)
 		p[i] = min;
 
 	return (p);
