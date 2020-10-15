5b82494390e88af52990e6dcfcb50e25d1c8f251
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-10-15 18:42:21 +0000

    change condition at if

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,15 +14,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 
 	/*check arguments*/
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	/*allocate memory*/
-	p = malloc(size * nmemb);
+	p = malloc(nmemb * size);
 	/*check memory*/
 	if (p == NULL)
 		return (NULL);
 	/*initialice memory to zero*/
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		p[i] = 0;
 	/*return pointer cast to void*/
 	return (p);
