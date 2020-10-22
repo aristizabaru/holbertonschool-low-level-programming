89bfa5e41ea40eb18f1d9bb6ab2f8da61d0de419
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-10-22 02:09:10 +0000

    task upload

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: array of intengers
+ * @size: size of the array
+ * @action: pointer fo function
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
