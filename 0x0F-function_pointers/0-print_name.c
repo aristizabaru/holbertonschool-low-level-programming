ad65f80ff4efa0eef454fa5765cb436ee7700d07
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-10-22 01:55:37 +0000

    task upload

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print a name
+ * @name: string of a name
+ * @f: pointer to function with char * as argument
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef __POINTERS__
+#define __POINTERS__
+#include <stdlib.h>
+#include <stdio.h>
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif
