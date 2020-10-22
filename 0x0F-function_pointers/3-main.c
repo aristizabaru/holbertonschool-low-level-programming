6c20b4656f0fe64beaff804e23a8032170e1744f
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-10-22 13:12:04 +0000

    fix ERROR output

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,31 +11,31 @@ int main(int argc, char *argv[])
 {
 	int num1 = 0, num2 = 0;
 
-	/*store numbers*/
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
 	/*checks*/
 	/*wrong number of arguments*/
 	if (argc != 4)
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(98);
 	}
 	/*wrong operator*/
 	/*check with get_op_func if it gets back NULL*/
 	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(99);
 	}
 	/*check if users try to / or % by 0*/
 	/*target the first character of string for comparation*/
-	if (num2 == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
+	if (argv[3][0] == '0' && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
-		printf("ERROR\n");
+		printf("Error\n");
 		exit(100);
 	}
+
+	/*store numbers*/
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
 
 	/*send string and get back address of function*/
 	printf("%d\n", get_op_func(argv[2])(num1, num2));
