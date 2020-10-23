d4e9248e9582b81ba362dbe6c051a2df7328eae5
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-10-23 21:45:22 +0000

    change logic of task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,46 @@
 #include "variadic_functions.h"
 
 /**
+ * printIntenger - print an interger
+ * @pa: argument to print
+ *
+ * Return: nothing
+ */
+void printIntenger(va_list pa)
+{
+	printf("%d", va_arg(pa, int));
+}
+
+/**
+ * printFloat -  prints a float
+ * @pa: argument to print
+ *
+ * Return: nothing
+ */
+void printFloat(va_list pa)
+{
+	printf("%f", va_arg(pa, double));
+}
+
+/**
+ * printString -  print a string
+ * @pa: argument to print
+ *
+ * Return: nothing
+ */
+void printString(va_list pa)
+{
+	char *string = va_arg(pa, char *);
+
+	if (string == NULL)
+	{
+		printf("%s", "(nil)");
+		return;
+	}
+	printf("%s", string);
+}
+
+/**
  * print_all -  prints anything
  * @format: options of formating
  * Description: if the string is NULL prints (nil)
@@ -11,41 +51,30 @@
 void print_all(const char *const format, ...)
 {
 	va_list pa;
-	unsigned int i = 0;
-	char *string = NULL;
-	int print;
+	/*create array of formats (code + function)*/
+	formato formato[] = {{'c', printChar},
+			     {'i', printIntenger},
+			     {'f', printFloat},
+			     {'s', printString}};
+	unsigned int i = 0, j;
+
 	/*start list*/
 	va_start(pa, format);
 	/*check arguments and print*/
 	while (*(format + i))
 	{
-		print = 1;
-		switch (*(format + i))
+		j = 0;
+		while (j < 4)
 		{
-		case 'c':
-			printf("%c", va_arg(pa, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(pa, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(pa, double));
-			break;
-		case 's':
-			string = va_arg(pa, char *);
-			if (string == NULL)
+			if (formato[j].code == *(format + i))
 			{
-				printf("%s", "(nil)");
-				break;
+				formato[j].print(pa);
+				/*check conditions to print ,*/
+				if (*(format + (i + 1)) != '\0')
+					printf(", ");
 			}
-			printf("%s", string);
-			break;
-		default:
-			print = 0;
+			j++;
 		}
-		/*check conditions to print ,*/
-		if (*(format + (i + 1)) != '\0' && print)
-			printf(", ");
 		i++;
 	}
 	printf("\n");
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,21 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void printString(va_list pa);
+void printFloat(va_list pa);
+void printIntenger(va_list pa);
+void printChar(va_list pa);
+/**
+ * struct formato - format code and functions
+ * @code: format code
+ * @print: function pointer to print format
+ *
+ * Description: data to evaluate how to print
+ */
+struct formato
+{
+	char code;
+	void (*print)(va_list);
+};
+typedef struct formato formato;
 #endif
