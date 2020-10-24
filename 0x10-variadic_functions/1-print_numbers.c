ac60ef4d123cc16259d02ce18dea162b41d664bb
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-10-24 19:54:41 +0000

    fix separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,19 +13,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list pa;
 
-	if (separator == NULL)
-	{
-		va_end(pa);
-		return;
-	}
 	va_start(pa, n);
 	for (i = 0; i < n; i++)
 	{
 
 		printf("%d", va_arg(pa, unsigned int));
-		if (i + 1 != n)
+		if (i + 1 != n && separator != NULL)
 			printf("%c ", separator[0]);
 	}
 	printf("\n");
 	va_end(pa);
 }
+
