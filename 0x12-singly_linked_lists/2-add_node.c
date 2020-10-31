dd176e7213a51ce4c330ea349bfed956debb44f1
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-10-31 02:37:04 +0000

    remove print_list function

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,28 +14,6 @@ unsigned int get_string_length(char *str)
 		length++;
 	return (length);
 }
-/**
- * print_list - prints all the elements of a list_t list
- * @h: head of the list
- * Description: if str is NULL, print [0] (nil)
- *
- * Return: number of nodes
- */
-size_t print_list(const list_t *h)
-{
-	size_t count = 0;
-
-	while (h)
-	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
-		count++;
-	}
-	return (count);
-}
 
 /**
  * add_node - adds a new node at the beginning of a list_t list
