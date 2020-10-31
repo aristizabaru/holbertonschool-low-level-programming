a12b76c6ea73cf51b0480fba8d47facf3722dee0
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-10-31 02:15:38 +0000

    01 list.h - 02 2-add_node.c-m01 add prototype of get_string_length - 02 create function to add node at the beginning of list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,24 +14,47 @@ unsigned int get_string_length(char *str)
 		length++;
 	return (length);
 }
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: head of the list
+ * Description: if str is NULL, print [0] (nil)
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
 
 /**
- * add_node - adds a new node at the beginning of a list.
- * @head: double pointer to head of node
- * @str: data to be added to node
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: head of the list
+ * @str: string to fill member str
+ * Description: str needs to be duplicated
  *
  * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *nodePtr = NULL;
+	list_t *temp = NULL;
 
-	nodePtr = malloc(sizeof(list_t));
-	if (nodePtr == NULL)
+	temp = malloc(sizeof(list_t));
+	if (temp == NULL)
 		return (NULL);
-	nodePtr->str = strdup(str);
-	nodePtr->len = get_string_length(nodePtr->str);
-	nodePtr->next = *head;
-	*head = nodePtr;
-	return (nodePtr);
+	temp->str = strdup(str);
+	temp->len = get_string_length(temp->str);
+	temp->next = *head;
+	*head = temp;
+	return (*head);
 }
