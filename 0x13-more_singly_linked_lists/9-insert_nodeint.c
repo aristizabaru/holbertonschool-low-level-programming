7644a3cbfece8c34b53ac92cd6daa5660faef1c1
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-11-04 12:54:55 +0000

    free new_node if index is invalid

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 		if (temp == NULL)
 		{
-			free(temp);
+			free(new_node);
 			return (NULL);
 		}
 		count++;
