0e890ddbab5e8e71233e1c5a8acd6cec0e89fb35
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-11-04 13:04:40 +0000

    add check for memory allocation

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL)
 		return (NULL);
 	new_node = malloc(sizeof(*new_node));
+	if (new_node == NULL)
+		return (NULL);
 	new_node->n = n;
 	new_node->next = NULL;
 	if (*head == NULL && idx == 0)
