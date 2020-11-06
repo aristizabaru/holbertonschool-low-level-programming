3ac88536048d6af4300faf45d65d0bec6bd55e1e
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-11-06 02:58:01 +0000

    file upload

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - convert a binary to an unsigned int
+ * @b: binary number
+ * Return: number or 0 if b is not a binary
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int total, power;
+	int len;
+
+	if (b == NULL)
+		return (0);
+
+	for (len = 0; b[len]; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+	}
+
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			total += power;
+	}
+
+	return (total);
+}
