c4f43c646da362ba7da9e43176e8e5b782d064d5
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-11-10 11:53:15 +0000

    refine logic

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,41 @@
 #include "holberton.h"
 
 /**
- * create_file - create a file with read/write access for user
- * @filename: name of file to create
- * @text_content: string to write to file
+ * create_file - creates a file
+ * @filename: new file name
+ * @text_content: string to fill the file
+ *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, rstatus, i;
+	int result = 1, fd = 0, i = 0, length = 0;
 
+	/*check for file name and string*/
 	if (filename == NULL)
-		return (-1);
-
+	{
+		result = -1;
+		return (result);
+	}
+	/*create file with permissions if tis new*/
+	/*truncate file to 0 chars*/
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
-		return (-1);
-
+	{
+		result = -1;
+		return (result);
+	}
+	/*check if there's a string*/
 	if (text_content)
 	{
-		for (i = 0; text_content[i] != '\0'; i++)
-			;
-		rstatus = write(fd, text_content, i);
-		if (rstatus == -1)
-			return (-1);
+		/*find string lenght*/
+		while (text_content[i])
+			i++;
+		/*write text into file*/
+		length = write(fd, text_content, i);
+		if (length != i)
+			result = -1;
 	}
-
 	close(fd);
-	return (1);
+	return (result);
 }
