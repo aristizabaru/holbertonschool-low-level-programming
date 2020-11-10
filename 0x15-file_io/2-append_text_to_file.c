acb3841c60c2541d70a4f076f44e84379edbb01e
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-11-10 14:02:37 +0000

    change logic

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,30 +1,41 @@
 #include "holberton.h"
 
 /**
- * append_text_to_file - Append text to the end of a file
+ * create_file - appends text at the end of a file
  * @filename: name of file
- * @text_content: string to add to end of file
+ * @text_content: string to add to file
+ *
  * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, status, i;
+	int result = 1, fd = 0, i = 0, length = 0;
 
+	/*check parameters*/
 	if (filename == NULL)
-		return (-1);
-	if (text_content == NULL)
-		return (1);
-
-	fd = open(filename, O_APPEND | O_WRONLY);
+	{
+		result = -1;
+		return (result);
+	}
+	/*open file*/
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
-		return (-1);
+	{
+		result = -1;
+		return (result);
+	}
+	/*fill content to file*/
+	if (text_content && fd > 0)
+	{
+		/*find string lenght*/
+		while (text_content[i])
+			i++;
+		/*write text into file*/
+		length = write(fd, text_content, i);
+		if (length != i)
+			result = -1;
+	}
 
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	status = write(fd, text_content, i);
-	if (status == -1)
-		return (-1);
-
-	close(fd);
-	return (1);
+	/*close File Descriptor*/
+	return (result);
 }
