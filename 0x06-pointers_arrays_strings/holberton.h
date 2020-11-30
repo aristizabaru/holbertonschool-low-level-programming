8cd42f07acfb71081b2cd50a6b67289c8a81b3ac
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-11-30 11:36:03 +0000

    Style(betty)

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -11,3 +11,5 @@ char *rot13(char *);
 void print_number(int n);
 void print(int l, int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_line(char *c, int s, int l);
+void print_buffer(char *b, int size);
