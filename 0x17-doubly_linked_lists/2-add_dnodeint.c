5910027aa04b8a940252d712946d747351ac5a95
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-12-13 11:47:58 +0000

    Stye(Betty): add name for parameter **head in prototype description

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,7 +2,7 @@
 
 /**
  * add_dnodeint - adds a new node at the beginning of a dlistint_t list
- * @h: head of the list
+ * @head: head of the list
  * @n: new node data
  * Return: the address of the new element, or NULL if it failed
  */
