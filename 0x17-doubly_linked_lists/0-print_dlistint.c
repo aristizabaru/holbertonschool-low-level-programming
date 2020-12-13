b79dab733567bbd1d00a70f08df6408ba440a6a5
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-12-13 11:41:57 +0000

    Test(no list aviable): check if head variable is NULL

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,11 +9,15 @@ size_t print_dlistint(const dlistint_t *h)
 {
     size_t node_count = 0;
 
-    while (h)
+    if (h)
     {
-        printf("%d\n", h->n);
-        node_count++;
-        h = h->next;
+        while (h)
+        {
+            printf("%d\n", h->n);
+            node_count++;
+            h = h->next;
+        }
     }
+
     return (node_count);
 }
