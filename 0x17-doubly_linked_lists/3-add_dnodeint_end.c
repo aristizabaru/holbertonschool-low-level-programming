17783b9323b64dba6d264b5d7194cc0fcd4adcd4
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-12-13 11:49:41 +0000

    Stye(Betty): add name for parameter **head in prototype description and add \n at the end of file

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,7 +2,7 @@
 
 /**
  * add_dnodeint_end - adds a new node at the end of a dlistint_t list
- * @h: head of the list
+ * @head: head of the list
  * @n: new node data
  * Return: the address of the new element, or NULL if it failed
  */
