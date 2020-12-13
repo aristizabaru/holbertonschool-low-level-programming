04adf16ae09ad602ff0a8f69bdf30bd911641b39
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-12-13 11:59:34 +0000

    Style(Betty): Delete comments and rewrite prototype description

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index index of a dlistint_t linked list
+ * delete_dnodeint_at_index - deletes the node at index in a dlistint_t list
  * @head: head of the list
  * @index: index where node should be deleted, starting from 0
  * Return: 1 if it succeeded, -1 if it failed
@@ -9,8 +9,7 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *temp = NULL;
-	/**
-     * revisar lógica en white board y hacer suitcase */
+
 	if (*head)
 	{
 		if ((*head)->next == NULL && index == 0)
