2a9e929132d50017dae1891040807d77945bea8b
Andres Aristizabal <andres.aristizabal@gmail.com>
2020-12-15 01:08:08 +0000

    Style(Betty): add new line at EOF

--- 0x18-dynamic_libraries/holberton.h
+++ 0x18-dynamic_libraries/holberton.h
@@ -1,5 +1,8 @@
 #ifndef __HOLBERTON__H__
 #define __HOLBERTON__H__
+
+#define EXIT_SUCCESS 0
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,5 +24,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#define EXIT_SUCCESS 0
 #endif
