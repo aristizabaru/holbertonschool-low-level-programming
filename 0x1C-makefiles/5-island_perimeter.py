98a00d1db0f1960cd137868156483c19902e946a
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-02-20 01:04:29 +0000

    Fix: remove logic to take into acount diagonally conections

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,7 +12,6 @@ def island_perimeter(grid):
     perimeter = 0
     max_row = 0
     max_col = 0
-    row_col = [0, 0]
 
     if grid:
         for row_idx, row in enumerate(grid):
@@ -22,14 +21,8 @@ def island_perimeter(grid):
             suma_col = 0
             for col in range(0, len(grid)):
                 suma_col += grid[col][row_idx]
-                if grid[row_idx][col] == 1 and row_idx + col > sum(row_col):
-                    row_col[0] = row_idx
-                    row_col[1] = col
             if suma_col > max_col:
                 max_col = suma_col
+        perimeter = (max_row*2) + (max_col*2)
 
-        if grid[row_col[0] - (max_row-1)][max_col] == 1:
-            perimeter = max_row + max_col
-        else:
-            perimeter = max_col + max_row + (max_col + max_row)
     return perimeter
