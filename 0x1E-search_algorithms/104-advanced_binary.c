041fb8c5bc62dcf2b71f3ac3d3b6fdad2fd4c96d
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-04-21 19:50:40 +0000

    Docs: add function name to binarySearchRecursive

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary - find value in a array
+ * binarySearchRecursive - find value in a array
  * @array: sorted values
  * @left: left bound of the array
  * @right: right bound of the array
