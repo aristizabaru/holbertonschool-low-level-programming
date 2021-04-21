0631b59db7087e24e2153737fbe019609c168895
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-04-21 12:54:56 +0000

    Feat: look for first index in the array that match value

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -30,9 +30,9 @@ int interpolation_search(int *array, size_t size, int value)
 			high = mid - 1;
 		else
 		{
-			if (value == array[mid - 1])
-				return (mid - 1);
-			return (mid);
+			for(; array[mid] == value; mid-- )
+			;
+			return (mid + 1);
 		}
 	}
 	if (value == array[low])
