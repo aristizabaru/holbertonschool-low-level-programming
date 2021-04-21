06dc261ae0a44f7c8dba98d26cb2b85bbf17a306
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-04-21 10:32:09 +0000

    First commit

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,39 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - find value in a array
+ * @array: sorted values
+ * @size: size of the array
+ * @value: value to be found
+ *
+ * Return: index of value on success, -1 on failure
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	unsigned int left = 0;
+	/* calculates intial jump */
+	unsigned int right = 0;
+	unsigned int i = 0;
+
+	if (!array)
+		return (-1);
+	while (array[right] < value && right < size)
+	{
+		printf("Value checked array[%d] = [%d]\n", right, array[right]);
+		/* move one jump if it is not the correct block */
+		left = right;
+		right += sqrt(size);
+		/* if right exceeds then bound the range */
+	}
+	printf("Value found between indexes [%d] and [%d]\n", left, right);
+	if (right > size - 1)
+		right = size - 1;
+	for (i = left; i <= right; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		/* linear search when correct block found */
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,37 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search- find value in a array
+ * @array: sorted values
+ * @size: size of the array
+ * @value: value to be found
+ *
+ * Return: index of value on success, -1 on failure
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	int low = 0;
+	int high = size - 1;
+	int mid;
+
+	if (!array)
+		return (-1);
+
+	while ((array[high] != array[low]) && (value >= array[low])
+		&& (value <= array[high]))
+	{
+		mid =  low + (((double)(high - low) / (array[high] - array[low]))
+				* (value - array[low]));
+		printf("Value checked array[%d] = [%d]\n", mid, array[mid]);
+		if (array[mid] < value)
+			low = mid + 1;
+		else if (value < array[mid])
+			high = mid - 1;
+		else
+		return (mid);
+	}
+	if (value == array[low])
+		return (low);
+	else
+		return (-1);
+}
