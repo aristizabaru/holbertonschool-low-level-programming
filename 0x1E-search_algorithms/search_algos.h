3e5b584c7b700c4210c3db197b13ea353e69e557
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-04-21 15:55:46 +0000

    Add prototypes for exponential search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,6 +9,8 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int binary_search_explonential(int *array, int left, int right, int value);
 int advanced_binary(int *array, size_t size, int value);
 
 #endif /* __SEARCH__H__ */
