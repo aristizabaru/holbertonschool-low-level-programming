d2b8b6349d38825925523180e6a1bbc06fa19192
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-04-21 10:29:00 +0000

    Feat: add check if value is present

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,8 +1,8 @@
 #include "search_algos.h"
 
 /**
- * linear_search - find value using in array
- * @array: random values
+ * binary_search - find value in a array
+ * @array: sorted values
  * @size: size of the array
  * @value: value to be found
  *
@@ -10,28 +10,29 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-        int left = 0;
-        int right = size - 1;
-        int mid = NULL;
-        int i = 0;
+	int left = 0;
+	int right = size - 1;
+	int mid = NULL;
+	int i = 0;
 
-        while (left < right)
-        {
-                mid = (left + right) / 2;
-                printf("Searching in array: ");
-                for (i = left; i < right; i++)
-                        printf("%d, ", array[i]);
-                printf("%d\n", array[i]);
-                if (array[mid] == value)
-                        return (mid);
-                else if (value < array[mid])
-                        right = mid - 1;
-                else
-                        left = mid + 1;
-
-        }
-        printf("Searching in array: %d\n", array[right]);
-        if (array[right] == value)  
-                return (array[right]);
-        return (-1);
+	if (!array || !value)
+		return (-1);
+	while (left < right)
+	{
+		mid = (left + right) / 2;
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+		if (array[mid] == value)
+			return (mid);
+		else if (value < array[mid])
+			right = mid - 1;
+		else
+			left = mid + 1;
+	}
+	printf("Searching in array: %d\n", array[right]);
+	if (array[right] == value)
+		return (array[right]);
+	return (-1);
 }
