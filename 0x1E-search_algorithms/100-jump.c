b7329814047a8dbeb6c0d6c383d137f86ec2cd70
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-04-21 11:34:10 +0000

    Fix: add check on size

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -15,7 +15,7 @@ int jump_search(int *array, size_t size, int value)
 	unsigned int right = 0;
 	unsigned int i = 0;
 
-	if (!array)
+	if (!array || !size)
 		return (-1);
 
 	while (array[right] < value && right < size)
@@ -38,4 +38,3 @@ int jump_search(int *array, size_t size, int value)
 	}
 	return (-1);
 }
-
