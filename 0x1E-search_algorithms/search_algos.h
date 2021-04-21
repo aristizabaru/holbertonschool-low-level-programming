30b505f6dfe2add33ec468a62e9509588a032689
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-04-21 10:22:24 +0000

    Style(betty): add new line to end of file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,9 +2,13 @@
 #define __SEARCH__H__
 
 #include <stdio.h>
+#include <math.h>
 
 /* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif /* __SEARCH__H__ */
