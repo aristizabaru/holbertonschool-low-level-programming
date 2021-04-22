e4ac44dd7ded09446fd8ba1c4c059f3b99ce89e3
Andres Aristizabal <andres.aristizabal@gmail.com>
2021-04-22 01:24:35 +0000

    Feat: prototypes for jum_list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,23 @@
 #include <stdio.h>
 #include <math.h>
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+   int n;
+   size_t index;
+   struct listint_s *next;
+} listint_t;
+
 /* PROTOTYPES */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -12,5 +29,6 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int binary_search_explonential(int *array, int left, int right, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 #endif /* __SEARCH__H__ */
